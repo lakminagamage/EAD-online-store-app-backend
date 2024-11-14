5fc4bb2c899f3d3c249d63681714cb720e6b62c4
Nethmi Rathnayake <108873206+NethmiR@users.noreply.github.com>
2024-11-14 09:49:34 +0530

    feat(user update)- made the createdAT and UpdatedAT attributes change when the model got chaged or an instance was made from the model class

--- user-service/src/main/java/com/ead/user_service/model/User.java
+++ user-service/src/main/java/com/ead/user_service/model/User.java
@@ -38,4 +38,15 @@ public class User {
 
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }

6f8bb9ccd3c6e78b38093779b7a663193b10e65c
Isuranga Warnasooriya <IsurangaWarnasooriya@outlook.com>
2024-11-24 15:48:26 +0530

    create file uploading in which product creation

--- api-gateway/src/config/index.ts
+++ api-gateway/src/config/index.ts
@@ -6,4 +6,6 @@ export const config = {
   productServiceUrl: process.env.PRODUCT_SERVICE_URL as string,
   orderServiceUrl: process.env.ORDER_SERVICE_URL as string,
   userServiceUrl: process.env.USER_SERVICE_URL as string,
+  paymentServiceUrl: process.env.PAYMENT_SERVICE_URL as string,
+  fileServiceUrl: process.env.FILE_SERVICE_URL as string,
 };
--- api-gateway/src/routes/product.ts
+++ api-gateway/src/routes/product.ts
@@ -1,8 +1,69 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import { config } from "../config";
+import multer from "multer";
+import axios from "axios";
+import FormData from "form-data";
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post(
+  "/files",
+  upload.array("images"),
+  async (req, res): Promise<void> => {
+    try {
+      if (req.method !== "POST") {
+        res.status(400).json({ message: "Method not allowed" });
+        return;
+      }
+
+      const files = req.files as Express.Multer.File[];
+      if (!files || files.length === 0) {
+        res.status(400).json({ message: "No images uploaded" });
+      }
+
+      const formData = new FormData();
+      files.forEach((file) => {
+        formData.append("files", file.buffer, file.originalname);
+      });
+
+      const uploadResponse = await axios.post(
+        `${config.fileServiceUrl}/files/upload/multiple`,
+        formData,
+        {
+          headers: {
+            ...formData.getHeaders(),
+          },
+        }
+      );
+
+      const imageUrls = uploadResponse.data.urls.map((url: string) => ({
+        url,
+      }));
+
+      const productData = {
+        name: req.body.name,
+        description: req.body.description,
+        price: parseFloat(req.body.price),
+        stock: parseInt(req.body.stock),
+        product_type_id: parseInt(req.body.product_type_id),
+        images: imageUrls,
+      };
+
+      const productResponse = await axios.post(
+        `${config.productServiceUrl}/products/`,
+        productData
+      );
+
+      res.status(productResponse.status).json(productResponse.data);
+    } catch (error) {
+      console.error("Error creating product:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+    return;
+  }
+);
 
 // Proxy for Product-related routes
 router.use(
